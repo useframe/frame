d7c2b238a5ab967cac26d893feb2e59c21bffd16
Ayush Soni <ayushsoni1010.work@gmail.com>
2026-01-02 23:20:35 +0530

    feat(marketing): add blog pages
    - Add blog listing page
    - Add blog detail page with dynamic routing
    - Support blog content display

---
+++ src/app/(marketing)/blog/[blogId]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BlogPage = async ({ params }: { params: Promise<{ blogId: string }> }) => {
+  const { blogId } = await params;
+  console.log(blogId);
+  return <div>BlogPage</div>;
+};
+
+export default BlogPage;
